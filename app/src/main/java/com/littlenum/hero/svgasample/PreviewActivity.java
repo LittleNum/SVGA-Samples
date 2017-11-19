f29a4a8484d4e9b3da59764374e558cbea99a91a
LittleNum <infinityengine@163.com>
2017-11-19 20:11:48 +0800

    @Author: LittleNum @Review: Description: delete gson,protobuf can't be transformed to json directly

--- app/src/main/java/com/littlenum/hero/svgasample/PreviewActivity.java
+++ app/src/main/java/com/littlenum/hero/svgasample/PreviewActivity.java
@@ -7,12 +7,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextPaint;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
 import com.opensource.svgaplayer.SVGADrawable;
 import com.opensource.svgaplayer.SVGADynamicEntity;
 import com.opensource.svgaplayer.SVGAImageView;
@@ -46,9 +44,6 @@ public class PreviewActivity extends AppCompatActivity {
                         SVGADrawable drawable = new SVGADrawable(mSVGAVideoEntity);
                         mSVGAImageView.setImageDrawable(drawable);
                         mSVGAImageView.startAnimation();
-                        Gson gson = new Gson();
-                        String data = gson.toJson(mSVGAVideoEntity);
-                        Log.e("gson", data);
                     }
 
                     @Override
